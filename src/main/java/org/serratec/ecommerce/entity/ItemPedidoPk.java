68d1b0f1ac1078b82fd6b77a39200f1613ed0ac3
natalino <natalinoesteves2010@gmail.com>
2021-11-16 17:21:24 -0300

    add pedido e pedido fk 1.0

--- src/main/java/org/serratec/ecommerce/entity/ItemPedidoPk.java
+++ src/main/java/org/serratec/ecommerce/entity/ItemPedidoPk.java
@@ -7,6 +7,7 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+//ligacao n:n
 @Embeddable
 public class ItemPedidoPk implements Serializable {
 
