e498a90fc02939cee5fc26298ea2e279d3d5ef05
Lucas Esteves <lucasesteves5304@gmail.com>
2021-11-18 11:20:26 -0300

    Add excpetion

--- src/main/java/org/serratec/ecommerce/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/org/serratec/ecommerce/controller/exception/ControllerExceptionHandler.java
@@ -6,6 +6,8 @@ import org.serratec.ecommerce.services.exceptions.DataIntegrityException;
 import org.serratec.ecommerce.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,7 +30,20 @@ public class ControllerExceptionHandler {
 	}
 	
 	
-	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validacao(MethodArgumentNotValidException e , HttpServletRequest request){
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+		for (FieldError x : e.getBindingResult().getFieldErrors())
+		{
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
 	
 	
 	
