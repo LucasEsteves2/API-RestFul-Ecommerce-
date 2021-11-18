6fea4ed3805ee765aa1ceb827118c4821fc934e4
natalino <natalinoesteves2010@gmail.com>
2021-11-18 11:50:07 -0300

    FieldMessage terminada

--- src/main/java/org/serratec/ecommerce/controller/exception/FieldMessage.java
+++ src/main/java/org/serratec/ecommerce/controller/exception/FieldMessage.java
@@ -9,5 +9,30 @@ public class FieldMessage implements Serializable {
 	private String fieldName;
 	private String message;
 
-	
+	public FieldMessage() {
+
+	}
+
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 }
