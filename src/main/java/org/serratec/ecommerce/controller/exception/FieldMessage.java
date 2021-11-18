24031127baab8a4c5813449015059d7f61c27532
natalino <natalinoesteves2010@gmail.com>
2021-11-18 11:49:43 -0300

    add Exception FieldMessage

---
+++ src/main/java/org/serratec/ecommerce/controller/exception/FieldMessage.java
@@ -0,0 +1,13 @@
+package org.serratec.ecommerce.controller.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 7989517047543292493L;
+
+	private String fieldName;
+	private String message;
+
+	
+}
