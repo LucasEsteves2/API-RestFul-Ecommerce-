9f5343fbcf189484fc84f858a7dda44d16328d95
Lucas Esteves <lucasesteves5304@gmail.com>
2021-11-22 15:25:27 -0300

    ultimos ajustes

--- src/main/java/org/serratec/ecommerce/ApiRestSerratecApplication.java
+++ src/main/java/org/serratec/ecommerce/ApiRestSerratecApplication.java
@@ -1,7 +1,5 @@
 package org.serratec.ecommerce;
 
-import org.serratec.ecommerce.services.S3Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/org/serratec/ecommerce/dto/PedidoDTO.java
+++ src/main/java/org/serratec/ecommerce/dto/PedidoDTO.java
@@ -1,22 +1,7 @@
 package org.serratec.ecommerce.dto;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
-import org.serratec.ecommerce.entity.Cliente;
-import org.serratec.ecommerce.entity.Endereco;
-import org.serratec.ecommerce.entity.ItemPedido;
 import org.serratec.ecommerce.entity.Pagamento;
-import org.serratec.ecommerce.entity.Pedido;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class PedidoDTO {
 
